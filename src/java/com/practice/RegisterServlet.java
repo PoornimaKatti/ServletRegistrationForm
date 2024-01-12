ee20be10a44a210ef762d01c554b75257bde8a59
Program <Program@example.com>
2024-01-12 12:18:47 +0530

    first commit

---
+++ src/java/com/practice/RegisterServlet.java
@@ -0,0 +1,49 @@
+
+package com.practice;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.*;
+import  javax.servlet.http.*;
+
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h2>Welcome to Register Servlet</h2>");
+        //to store in containter we take string
+       String name = req.getParameter("user_name");
+       String password = req.getParameter("user_password");
+       String email = req.getParameter("user_email");
+       String gender = req.getParameter("user_gender");
+       String course = req.getParameter("user_course");
+       
+       String cond=req.getParameter("condition");
+   
+       out.println(cond);
+       
+      
+          if(cond.equals("checked"))
+           {
+              out.println("<h2> Name:"+name+"<h2>");
+              out.println("<h2> Password:"+password+"<h2>");
+              out.println("<h2> Email:"+email+"<h2>");
+              out.println("<h2> Gender:"+gender+"<h2>");
+              out.println("<h2> Course:"+course+"<h2>");
+              
+              
+          }else{
+              out.println("<h2>you have not accepted terms and condition</h2>");
+              
+          }
+              
+      }
+  }
+          
+          
+       
+
+    
+    
+
